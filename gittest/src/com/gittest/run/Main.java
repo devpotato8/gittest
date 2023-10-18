fb68e4a145eb16e43cf0f8071f310d0a8490a729
sidty <sidty@DESKTOP-GSF0T8P>
2023-10-18 09:12:51 +0900

    bs_main 메소드 수정

--- gittest/src/com/gittest/run/Main.java
+++ gittest/src/com/gittest/run/Main.java
@@ -1,11 +1,11 @@
 package com.gittest.run;
 
+import com.gittest.controller.GitController;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("안녕 반가워");
-		System.out.println("-DEV브랜치-");
-
+		new GitController().test();
 	}
 
 }
